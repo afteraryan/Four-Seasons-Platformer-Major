af8b470d45fb880e56086dbaaeed9a4d4f30c3cf
Aryan Saxena <129835171+playschoolstudios@users.noreply.github.com>
2023-08-26 02:06:18 +0530

    Jump Polished

--- Project File/U.FourSeasons/Assets/_Project/Player Character/Scripts/CharacterController.cs
+++ Project File/U.FourSeasons/Assets/_Project/Player Character/Scripts/CharacterController.cs
@@ -6,6 +6,8 @@ public class CharacterController : MonoBehaviour
 {
     [Header("Movement Parameters")]
     [SerializeField] private float moveSpeed;
+
+    private Vector2 Direction;
     
     [Header("Jump Parameters")]
     //[SerializeField] private float jumpForce;
@@ -14,6 +16,24 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float minMidJumpDuration = 2;
     [SerializeField] private float highJumpForce = 3;
     [SerializeField] private float minHighJumpDuration = 2;
+    [SerializeField] private float horizontalJumpBoost = 2.0f; // Boost in horizontal velocity during jump
+    [SerializeField] private float jumpApexBoostDivideFactor = 6.0f; // Boost in horizontal velocity during jump
+    private float jumpCooldown = 0f;
+    private const float JumpCooldownDuration = 0.5f; // Adjust this value as needed
+    private float verticalForce = 0f;
+    
+    [Header("Fall Parameters")]
+    [SerializeField] private float jumpFallingForce = 1.5f;
+    [SerializeField] private float fallingTerminalVelocity = 1f; //Maximum Falling Velocity
+    private float fallDelayTimer = 0f;
+    private const float FallDelayDuration = 0.00f; // Adjust this value as needed
+    private float currentFallingForce = 0f;
+    private const float ForceLerpDuration = 1f; // Adjust this value as needed
+    private float forceLerpTimer = 0f;
+
+
+    private bool hasAppliedDownwardForce = false;
+    private bool hasAppliedHorizontalJumpBoost = false;
 
     [Header("Ground Check Parameters")]
     [SerializeField] private Transform groundCheckPoint;
@@ -49,8 +69,16 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (jumpCooldown > 0f)
+        {
+            jumpCooldown -= Time.fixedDeltaTime;
+        }
+
+        CalculateDirection();
         Move();
+        Fall();
     }
+
     
     private void CheckIfInAir()
     {
@@ -124,30 +152,121 @@ public class CharacterController : MonoBehaviour
     // ReSharper disable Unity.PerformanceAnalysis
     private void Move()
     {
-        // Only allow horizontal movement if not in air
+        // Only allow horizontal movement if not in air and not in jump cooldown
+        if (!isInAir && jumpCooldown <= 0f)
+        {
+            Vector2 velocity = new Vector2((input.Horizontal * moveSpeed), rb.velocity.y);
+            rb.velocity = velocity;
+            hasAppliedDownwardForce = false;  // Reset the flag when not in air
+        }
+    }
+
+    private void Fall()
+    {
         if (!isInAir)
         {
-            Vector2 velocity = new Vector2(input.Horizontal * moveSpeed, rb.velocity.y);
-            rb.velocity = velocity;
+            fallDelayTimer = 0f; // Reset the timer when not in air
+            forceLerpTimer = 0f; // Reset the force lerp timer
+            currentFallingForce = 0f; // Reset the current falling force
+            return;
         }
+    
+        bool isFalling = rb.velocity.y < 0;
 
-        
+        if (isFalling)
+        {
+            if (fallDelayTimer < FallDelayDuration)
+            {
+                fallDelayTimer += Time.fixedDeltaTime;
+                return; // Exit the function early, delaying the application of the downward force
+            }
+
+            if (!hasAppliedDownwardForce)
+            {
+                if (!hasAppliedHorizontalJumpBoost)
+                {
+                    rb.AddForce(new Vector2((verticalForce*Direction.x)/jumpApexBoostDivideFactor, 0), ForceMode2D.Impulse);
+                    hasAppliedHorizontalJumpBoost = true;
+                }
+                // Use the logarithmic function
+                if (forceLerpTimer < ForceLerpDuration)
+                {
+                    forceLerpTimer += Time.fixedDeltaTime;
+                    float a = jumpFallingForce / Mathf.Log(ForceLerpDuration + 1); // Adjust this value as needed
+                    float b = 3f; // Adjust this value as needed
+                    currentFallingForce = a * Mathf.Log(b * forceLerpTimer + 1);
+                    rb.AddForce(new Vector2(0, -currentFallingForce), ForceMode2D.Force);
+                }
+                else
+                {
+                    hasAppliedDownwardForce = true;  // Set the flag to true after applying the force
+                    //rb.AddForce(new Vector2(,0), ForceMode2D.Impulse);
+                }
+            }
+
+            if (rb.velocity.y < -fallingTerminalVelocity)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -fallingTerminalVelocity);
+            }
+        } 
     }
 
+
+
+    
     private void Jump(float timeDifference)
     {
-
+        verticalForce = 0f;
         if (timeDifference < minMidJumpDuration)
-            rb.AddForce(new Vector2(0, lowJumpForce), ForceMode2D.Impulse);
-        else if(timeDifference >= minMidJumpDuration && timeDifference< minHighJumpDuration )
-            rb.AddForce(new Vector2(0, midJumpForce), ForceMode2D.Impulse);
+            verticalForce = lowJumpForce;
+        else if(timeDifference >= minMidJumpDuration && timeDifference < minHighJumpDuration)
+            verticalForce = midJumpForce;
         else if(timeDifference >= minHighJumpDuration)
-            rb.AddForce(new Vector2(0, highJumpForce), ForceMode2D.Impulse);
+            verticalForce = highJumpForce;
 
-        input.JumpRequested = false;
+        // Calculate the horizontal force based on the direction of movement
+        float horizontalForce = input.Horizontal * horizontalJumpBoost;
         
-        Debug.Log("Time Difference: " + timeDifference);
+        // Apply the forces
+        rb.AddForce(new Vector2(horizontalForce, verticalForce), ForceMode2D.Impulse);
+        
+        jumpCooldown = JumpCooldownDuration;
+        hasAppliedHorizontalJumpBoost = false;
+        input.JumpRequested = false;
     }
+
+        
+    private void CalculateDirection()
+    {
+        // Determine X direction based on Rigidbody2D velocity
+        if (rb.velocity.x > 0)
+        {
+            Direction.x = 1; // Moving right
+        }
+        else if (rb.velocity.x < 0)
+        {
+            Direction.x = -1; // Moving left
+        }
+        else
+        {
+            Direction.x = 0; // No horizontal movement
+        }
+
+        // Determine Y direction based on Rigidbody2D velocity
+        if (rb.velocity.y > 0)
+        {
+            Direction.y = 1; // Moving up
+        }
+        else if (rb.velocity.y < 0)
+        {
+            Direction.y = -1; // Moving down
+        }
+        else
+        {
+            Direction.y = 0; // No vertical movement
+        }
+    }
+
 
     #endregion
 }
